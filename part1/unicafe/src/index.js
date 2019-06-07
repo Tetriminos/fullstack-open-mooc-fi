6beb7119ae55cb92687f448e5967e37d344d63a8
Domagoj Majcenic <dmajcenic@gmail.com>
2019-06-07 19:52:51 +0200

    Finish part1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Statistic = ({ text, value }) => (
+  <tr><td>{text}</td><td>{value}</td></tr>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = () => good + neutral + bad
+  const average = () => (1 * good + 0 * neutral + -1 * bad) / all()
+  const positive = () => `${good / all() * 100} %`
+
+  if (all() === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <Statistic text='good' value={good} />
+          <Statistic text='neutral' value={neutral} />
+          <Statistic text='bad' value={bad} />
+          <Statistic text='all' value={all()} />
+          <Statistic text='average' value={average()} />
+          <Statistic text='positive' value={positive()} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good'>good</Button>
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'>neutral</Button>
+      <Button handleClick={() => setBad(bad + 1)} text='bad'>bad</Button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
